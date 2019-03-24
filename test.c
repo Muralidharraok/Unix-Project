38cab6adeb7cdd8ea37c8b127e8eb7fc86af3a6f
Muralidharraok <muralidharraok15@gmail.com>
2019-03-24 12:50:43 +0530

    Added working code (All 127 char)

--- test.c
+++ test.c
@@ -6,24 +6,29 @@
 #include<fcntl.h>
 #include<unistd.h>
 int len,i,key_count=0,Variable_key;
-char message[1],ch;
+char message[10],ch;
 char *p;
 char encrypt(int key,char *message)
 {
-for(i=0; *message!='\0';i++)
-{
-  *message=*message+key;
-   message++;
-}
-  /*for(i = 0; *message[i] != '\0'; ++i)
+  for(i=0; i<len;i++)
   {
-     ch = message[i];
-     ch = ch + Variable_key;
-     printf("%c\n",ch);
-     //ch = ch - Variable_key;
-     message[i] = ch;
-  }*/
-  //return(message);
+    *message=(*message+key) % 127;
+     message++;
+  }
+}
+char decrypt(int key,char *message)
+{
+  for(i=0; i<len;i++)
+  {
+    int tempValue =0;
+    tempValue = *message-key;
+    if (tempValue < 0)
+    {
+      tempValue+=127;
+    }
+    *message=(tempValue) % 127;
+     message++;
+  }
 }
 int main(int argc, char *argv[])
 {
@@ -40,17 +45,32 @@ int main(int argc, char *argv[])
 
   for(i=0;i<len;i++)
   {
-    key_in_ASCII[i] = ((int)key[i])%26;
+    key_in_ASCII[i] = (int)key[i];
+  }
+
+  for(i = 0; i < len; i++) {
+    printf("%d\n", key_in_ASCII[i]);
   }
   int sfd=open(argv[3],O_RDONLY|O_EXCL,0);
-	int dfd=open(argv[4],O_WRONLY|O_CREAT,S_IRWXU|S_IRGRP|S_IWGRP|S_IROTH);
+  int dfd=open(argv[4],O_WRONLY|O_CREAT,S_IRWXU|S_IRGRP|S_IWGRP|S_IROTH);
   if (strcmp( argv[1], "-e" ) == 0 )
   {
     while(read(sfd,message,sizeof(message))>0)
-	  {
+    {
       p=&message[0];
       Variable_key = key_in_ASCII[key_count];
       encrypt(Variable_key,p);
+      write(dfd,message,sizeof(message));
+      key_count=(key_count+1)%len;
+    }
+  }
+  else if(strcmp( argv[1], "-d" ) == 0)
+  {
+    while(read(sfd,message,sizeof(message))>0)
+    {
+      p=&message[0];
+      Variable_key = key_in_ASCII[key_count];
+      decrypt(Variable_key,p);
       write(dfd,message,sizeof(message));
       key_count=(key_count+1)%len;
     }
